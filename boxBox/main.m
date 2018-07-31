e27403788c64783a2cd448d7cb6e25086e4d7700
Nathan Wainwright <nathanwainwright@gmail.com>
2018-07-31 11:37:31 -0700

    Initial Commit

---
+++ boxBox/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  boxBox
+//
+//  Created by Nathan Wainwright on 2018-07-31.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
